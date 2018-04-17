840e33707ea574d8f99a3f08481e0afd79b58769
pk1890 <piotrekkotara@gmail.com>
2018-04-17 15:11:50 +0200

    init commit

---
+++ Miller-Rabin/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Miller_Rabin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ilość bitów generowanych liczb pierwszych";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 150);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(263, 101);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 56);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4096,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Folder docelowy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(19, 14);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(246, 16);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Koparka liczb pierwszych";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Miller-Rabin/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//////////////////////////////////////////////////////////
+using System.Numerics; // :D #perfekcja
+using System.Security.Cryptography; // :D
+
+namespace Miller_Rabin
+{
+    public partial class Form1 : Form
+    {
+        bool kop = false;
+        BigInteger liczba = 3;
+        int bits_num;
+        StreamWriter plik;
+        bool Miller_Rabin(BigInteger n, int k) // zwraca prawdê, jesli liczba jest prawdopodobnie pierwsza
+{
+    Int32 s = 0;
+    BigInteger d, a;
+    BigInteger two = new BigInteger(2);
+    bool cont;
+    if(n%2 == 0) return false;
+    BigInteger q = n-1;
+    while(q%2 == 0)
+    {
+        s++;
+        q /= 2;
+    }
+    d = (n - 1) / BigInteger.Pow(two, s);
+    RandomNumberGenerator rand = RandomNumberGenerator.Create();
+    byte[] bytes = new byte[n.ToByteArray().LongLength];
+    for(int i = 0; i < k; i++)
+    {
+        cont = false;
+        do
+        {
+            rand.GetBytes(bytes);
+            a = new BigInteger(bytes);
+        } while (a < 2 || a >= n - 2);
+        BigInteger x = BigInteger.ModPow(a, d, n);
+      if(x == 1 || x == n - 1)
+        continue;
+ 
+      for(int r = 1; r < s; r++)
+      {
+        x = BigInteger.ModPow(x, 2, n);
+        if(x == 1)
+          return false;
+        if(x == n - 1)
+          break;
+      }
+ 
+      if(x != n - 1)
+        return false;
+    }
+
+    return true;
+}
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.SelectedPath == "")
+            {
+                button3_Click(sender, e);
+            }
+            numericUpDown1.ReadOnly = true;
+            numericUpDown1.Maximum = numericUpDown1.Value;
+            numericUpDown1.Minimum = numericUpDown1.Value;
+            button3.Enabled = false;
+            kop = true;
+            plik = new StreamWriter(@folderBrowserDialog1.SelectedPath + "\\liczby_pierwsze_" + Convert.ToString(numericUpDown1.Value) + "-bitowe.prime", true);
+            if (numericUpDown1.Value % 8 != 0)
+            {
+                numericUpDown1.Value = numericUpDown1.Value + (8-(numericUpDown1.Value%8));
+            }
+            bits_num = Convert.ToInt32(numericUpDown1.Value);
+            progressBar1.MarqueeAnimationSpeed = 25;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (kop == true)
+            {
+                RandomNumberGenerator rnd = RandomNumberGenerator.Create();
+                byte [] bytes = new byte[bits_num / 8];
+                do
+                {
+                    rnd.GetBytes(bytes);
+                    liczba = new BigInteger(bytes);
+                    liczba = BigInteger.Abs(liczba);
+                } while (liczba % 2 == 0);
+                if (Miller_Rabin(liczba, 20))
+                {
+                    richTextBox1.AppendText(liczba.ToString() + "\n\n");
+                    richTextBox1.ScrollToCaret();
+                    plik.WriteLine(liczba);
+                }
+               
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kop = false;
+            plik.Close();
+            progressBar1.MarqueeAnimationSpeed = 0;
+            numericUpDown1.ReadOnly = false;
+            numericUpDown1.Maximum = 4096;
+            numericUpDown1.Minimum = 1;
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+    }
+}
